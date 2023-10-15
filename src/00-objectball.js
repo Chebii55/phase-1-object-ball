76facc12c7b9efe7905bf77a2c472ec579a960e5
Sheila Chebii <shillerchier055@gmail.com>
2023-10-15 16:27:24 +0300

    done

---
+++ src/00-objectball.js
@@ -0,0 +1,163 @@
+function gameObject(){const gamers={
+    home:{
+        teamName:"Brooklyn Nets",
+        colors: "Black, White",
+        players:{"Alan Anderson":{
+            number:0,
+            shoe:16,
+            points:22,
+            rebounds:12,
+            assists:12,
+            steals:3,
+            blocks:1,
+            slamDunks:1,}
+        ,"Reggie Evans":{
+        number:30,
+        shoe:14,
+        points:12,
+        rebounds:12,
+        assists:12,
+        steals:12,
+        blocks:12,
+        slamDunks:7,}
+    ,"Brook Lopez":{
+    number:11,
+    shoe:17,
+    points:17,
+    rebounds:19,
+    assists:10,
+    steals:3,
+    blocks:1,
+    slamDunks:15,}
+,"Mason Plumlee":{
+number:1,
+shoe:19,
+points:26,
+rebounds:12,
+assists:5,
+steals:3,
+blocks:8,
+slamDunks:5,
+},"Jason Terry":{
+number:31,
+shoe:15,
+points:19,
+rebounds:2,
+assists:2,
+steals:4,
+blocks:11,
+slamDunks:1,}}
+
+    },
+    away:{teamName:"Charlotte Hornets",
+    colors: "Turquoise, Purple",
+    players:{
+        "Jeff Adrien":{
+            number:4,
+            shoe:18,
+            points:10,
+            rebounds:1,
+            assists:1,
+            steals:2,
+            blocks:7,
+            slamDunks:2,},
+            "Bismak Biyombo":{
+                number:0,
+                shoe:16,
+                points:12,
+                rebounds:4,
+                assists:7,
+                steals:7,
+                blocks:15,
+                slamDunks:10,},
+                "":{
+                    number:2,
+                    shoe:14,
+                    points:24,
+                    rebounds:12,
+                    assists:12,
+                    steals:4,
+                    blocks:5,
+                    slamDunks:5,},
+                    "Ben Gordon":{
+                        number:8,
+                        shoe:15,
+                        points:33,
+                        rebounds:3,
+                        assists:2,
+                        steals:1,
+                        blocks:1,
+                        slamDunks:0,},
+                        "Brendan Haywood":{
+                            number:33,
+                            shoe:16,
+                            points:6,
+                            rebounds:12,
+                            assists:12,
+                            steals:22,
+                            blocks:5,
+                            slamDunks:12,}
+    }
+}
+}
+return gamers}
+        
+function numPointsScored(playerName) {
+    const gamers = gameObject();
+    for (let team in gamers) {
+        for (let player in gamers[team].players) {
+            if (player === playerName) {
+                return gamers[team].players[player].points;
+            }
+        }
+    }
+    return "Player not found";
+}
+function shoeSize(playerName) {
+    const gamers = gameObject();
+    for (let team in gamers) {
+        for (let player in gamers[team].players) {
+            if (player === playerName) {
+                return gamers[team].players[player].shoe;
+            }
+        }
+    }
+    return "Player not found";
+}
+function teamColors(teamname){
+  const names = gameObject();
+    for (let name in names) {
+        if (names[name].teamName === teamname) {
+            return names[name].colors;
+        }
+    }
+    return null;  
+    /*  const names=gameObject();
+    for (let name in names) {
+        if (name === teamname) {
+            return names.teamName[teamname].colors;
+        }*/
+}
+function teamNames(){
+     const names = gameObject();
+    return [names.home.teamName, names.away.teamName];
+}
+function playerNumbers(){
+     const gamers = gameObject();
+    for (let team in gamers) {
+        if (gamers[team].teamName === teamName) {
+            const numbers = Object.values(gamers[team].players).map((player) => player.number);
+            return numbers;
+        }
+    }
+    return null; 
+}
+function playerStats(){
+      const gamers = gameObject();
+    for (let team in gamers) {
+        if (gamers[team].players.hasOwnProperty(playerName)) {
+            return gamers[team].players[playerName];
+        }
+    }
+    return null; 
+}
